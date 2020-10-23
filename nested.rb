4507bca2314cb9d9bbf34a508d067afbf4524727
Blaire Baker <bakerblaire@gmail.com>
2020-10-23 17:04:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-	alans_new_info = "Being a really nice guy"
+	alans_new_info = "GUI"
 	programmer_hash[:alan_kay][:known_for] = alans_new_info
 	programmer_hash
 end
